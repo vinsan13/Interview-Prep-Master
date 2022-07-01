2cd8c12c56088c618a57830d6311b8e9fb5a682f
Vinit Sanklecha <vinitsanklecha@gmail.com>
2022-07-01 15:32:47 +0530

    added new routes

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const cookieParser = require("cookie-parser");
+
+//session
 const session = require("express-session")
 const MongoDBStore = require("connect-mongodb-session")(session);
-const req = require("express/lib/request");
-const bodyParser = require("body-parser");
 
-const methodOverride = require("method-override");
-
+//routes
 const formRoutes = require("./routes/formRoutes");
 const authRoutes = require("./routes/authRoutes");
+
+//middleware
+const bodyParser = require("body-parser");
 const { isAuth, isNotAuth } = require("./middleware/authMiddleware");
 
 
-const  Topic  = require("./models/Topic");
-const Company= require("./models/Company");
-const  Experience  = require("./models/Experience");
-const  Question = require("./models/Question");
+//models
+const Topic = require("./models/Topic");
+const Company = require("./models/Company");
+const Experience = require("./models/Experience");
+const Question = require("./models/Question");
 
-
-
-
+//mongodb URI
 const dbURI =
   "mongodb+srv://admin-vinit:vinsan@cluster0.4zgtg.mongodb.net/InterviewPrepApp?retryWrites=true&w=majority";
 
-const store = new MongoDBStore({ uri: dbURI, collection: 'sessions' })
+
 //mongoose connect
 mongoose
   .connect(dbURI, {
@@ -32,21 +32,20 @@ mongoose
     useUnifiedTopology: true,
     useCreateIndex: true,
   })
-  .then(async() => {
+  .then(() => {
     console.log("mongoose connected");
-  
+
     appsetup();
   })
   .catch((err) => console.log("dberror vro:", err));
 
 
-//after mongoose connection is setup and database is extracted , appsetup will run
+//Appsetup
 const appsetup = () => {
   const app = express();
 
-
   const port = 3000;
-
+  // bind and listen the connections on the specified host and port.
   app.listen(process.env.PORT || 3000, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
@@ -55,12 +54,14 @@ const appsetup = () => {
   app.use(express.static("public"));
   app.use(bodyParser.urlencoded({ extended: false }));
 
-
   //views
   app.set("view engine", "ejs");
 
+  //sessions
+  const store = new MongoDBStore({ uri: dbURI, collection: 'sessions' })
   app.use(session({ secret: "my secret", resave: false, saveUninitialized: false, store: store }));
 
+  //middleware for storing session variables in res.locals which can be accessed by all pages
   app.use((req, res, next) => {
     res.locals.isLoggedIn = req.session.isLoggedIn;
     res.locals.user = req.session.user;
@@ -71,43 +72,63 @@ const appsetup = () => {
     next();
   })
 
+  //Home route
   app.get("/", (req, res) => {
     console.log("sucess nikkiiii :) :)");
     res.render("home");
   });
 
+
+  // Auth and form routes
   app.use(authRoutes);
   app.use(formRoutes);
 
-  //createAdminCumUser("email","username","password"); //to create admin cum user
 
-  //add topic route
-  app.get("/addtopic", (req, res) => {
+
+  //add and post topic
+  app.get("/addtopic",isAuth, (req, res) => {
     res.render('formTopic');
   })
 
-  app.post("/addtopic",async (req, res) => {
-    
+  app.post("/addtopic",isAuth, async (req, res) => {
+
     const t = await Topic.create({
-      name :req.body.topic
+      name: req.body.topic
     })
     res.redirect("/")
   })
 
+  // show all topics
+  app.get("/topics", isAuth, async (req, res) => {
+    const topics = await Topic.find({});
+    res.render("topics", { topics: topics });
 
-  app.get("/topics", isAuth, async(req, res) => {
-        const topics = await Topic.find({});
-        res.render("topics", { topics: topics });
-      
   });
 
+  // show questions of selected topic  
+  app.get("/topics/:t_name", isAuth, async (req, res) => {
 
-  app.get("/addcompany", (req, res) => {
+    let topic = req.params.t_name;
+    topic = topic.charAt(0).toUpperCase() + topic.slice(1); //making 1st char capital
+
+    //find topic  in topics collection
+    const foundtopic = await Topic.findOne({ name: topic.toString() })
+
+    //then get all questions linked to it
+    const questions = await Question.find({ topic: foundtopic._id })
+
+    //finally render the page
+    res.render("topic", { topic: topic, questions: questions });
+
+  });
+
+  //add and post company
+  app.get("/addcompany",isAuth, (req, res) => {
     res.render('formCompany');
   })
 
-  app.post("/addcompany",async (req, res) => {
-    const {name,link,image} = req.body;
+  app.post("/addcompany",isAuth, async (req, res) => {
+    const { name, link, image } = req.body;
     const c = await Company.create({
       name,
       link,
@@ -116,55 +137,35 @@ const appsetup = () => {
     res.redirect("/")
   })
 
-  app.get("/companys", isAuth,async (req, res) => {
+
+  // show all companies
+  app.get("/companys", isAuth, async (req, res) => {
     const companys = await Company.find({});
-        res.render("companys", { companys: companys });
-     
+    res.render("companys", { companys: companys });
   });
 
-  app.get("/topics/:t_name", isAuth,async (req, res) => {
-    //to convert arrays -> Arrays
-    let topic = req.params.t_name;
-    topic = topic.charAt(0).toUpperCase() + topic.slice(1);
-    //find topic  in topics collection
-    const foundtopic = await Topic.findOne({ name: topic.toString() })
-    
-    const questions  = await Question.find({topic: foundtopic._id})
-    
-      //finally render the page
-   
-      res.render("topic", { topic: topic, questions: questions });
-     
-  });
 
-  app.get("/companys/:c_name", isAuth,async (req, res) => {
-    //to convert arrays -> Arrays
+  // show interview experinces of selected company
+  app.get("/companys/:c_name", isAuth, async (req, res) => {
+
     let company = req.params.c_name;
     company = company.charAt(0).toUpperCase() + company.slice(1);
+    
     //find company  in companys collection
-    
-      const foundcompany = await Company.findOne({ name: company.toString() })
-      //then get all experiences linked to it
-      
-      const experiences = await Experience.find({ company: foundcompany._id })
-      
-      //finally render the page
-      
-        res.render("company", { company: company, experiences: experiences });
-      
-      
+    const foundcompany = await Company.findOne({ name: company.toString() })
+
+    //then get all experiences linked to it
+    const experiences = await Experience.find({ company: foundcompany._id })
+
+    //finally render the page
+    res.render("company", { company: company, experiences: experiences });
+
   });
-  app.get("/companys/:c_name/:expid", isAuth,async (req, res) => {
-  
+
+  // get new page for selected experince
+  app.get("/companys/:c_name/:expid", isAuth, async (req, res) => {
     let exp_id = req.params.expid;
-    
-    
-    
-      const foundexp = await Experience.findOne({ _id: exp_id })
-      
-      
-        res.render("experience", { experience: foundexp });
-      
-      
+    const foundexp = await Experience.findOne({ _id: exp_id });
+    res.render("experience", { experience: foundexp });
   });
 };
